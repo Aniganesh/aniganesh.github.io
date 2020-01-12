6c5497aafbd4c32e398681e3f2e268776aafedfb
Aniganesh <aniganesh741@gmail.com>
2020-01-12 21:40:45 +0530

    Added fonts. Added 5 other projects. Added one side padding, margin modifier functions. Changed descriptive picture
    Added fonts.
    Added 5 other projects.
    Added one side padding, margin modifier functionality.
    Changed descriptive picture
    Added published paper full text.

--- automated_work.py
+++ automated_work.py
@@ -4,3 +4,10 @@
             print(".col-"+str(i+1)+"-"+str(j+1)+"{grid-column-start:"+str(i+1)+ ";grid-column-end: "+str(j+1)+";}")
     
 '''
+for side in ['t', 'b', 'l', 'r']:
+    print("document.querySelectorAll(\"[class*=\'m-"+side+"-\']\");")
+    print("document.querySelectorAll(\"[class*=\'p-"+side+"-\']\");")
+
+'''
+print("/[p][\-]["+side+"][\-][0-9]/")
+print("/[m][\-]["+side+"][\-][0-9]/") '''
--- scripts/index.js
+++ scripts/index.js
@@ -43,3 +43,100 @@ marginSetitems.forEach((item) => {
     item.setAttribute("style", style);
 
 });
+
+const mtSetItems = document.querySelectorAll("[class*='m-t-']");
+mtSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[m][\-][t][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; margin-top: " + size + "em" : "margin-top: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const ptSetItems = document.querySelectorAll("[class*='p-t-']");
+ptSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[p][\-][t][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; padding-top: " + size + "em" : "padding-top: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const mbSetItems = document.querySelectorAll("[class*='m-b-']");
+mbSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[m][\-][b][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; margin-bottom: " + size + "em" : "margin-bottom: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const pbSetItems = document.querySelectorAll("[class*='p-b-']");
+pbSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[p][\-][b][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; padding-bottom: " + size + "em" : "padding-bottom: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const mlSetItems = document.querySelectorAll("[class*='m-l-']");
+mlSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[m][\-][l][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; margin-left: " + size + "em" : "margin-left: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const plSetItems = document.querySelectorAll("[class*='p-l-']");
+plSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[p][\-][l][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; padding-left: " + size + "em" : "padding-left: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const mrSetItems = document.querySelectorAll("[class*='m-r-']");
+mrSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[m][\-][r][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; margin-right: " + size + "em" : "margin-right: " + size + "em";
+    item.setAttribute("style", style);
+
+});
+const prSetItems = document.querySelectorAll("[class*='p-r-']");
+prSetItems.forEach((item) => {
+    const elementClass = item.className;
+    const classRegexp = /[p][\-][r][\-][0-9]/; // Look for a sequence starting with an 'm' followed by a hyphen (-) followed by one number.
+    let index = elementClass.search(classRegexp);
+    if (index < 0)
+        return;
+    const size = elementClass[index + 4];
+    const style = item.getAttribute("style") ? item.getAttribute("style") + "; padding-right: " + size + "em" : "padding-right: " + size + "em";
+    item.setAttribute("style", style);
+
+});
